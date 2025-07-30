51ab6d3fbd05b3545d3d941906eb87acb416cdb1
CBurg-College <cburgmicro@outlook.com>
2025-07-30 11:16:07 +0200

    pxt.json, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -3,38 +3,6 @@
 //% block.loc.nl="XGO Gamepad"
 namespace CXgoGamepad {
 
-    export enum Group {
-        //% block="group 1"
-        //% block.loc.nl="groep 1"
-        Group1,
-        //% block="group 2"
-        //% block.loc.nl="groep 2"
-        Group2,
-        //% block="group 3"
-        //% block.loc.nl="groep 3"
-        Group3,
-        //% block="group 4"
-        //% block.loc.nl="groep 4"
-        Group4,
-        //% block="group 5"
-        //% block.loc.nl="groep 5"
-        Group5,
-        //% block="group 6"
-        //% block.loc.nl="groep 6"
-        Group6,
-        //% block="group 7"
-        //% block.loc.nl="groep 7"
-        Group7,
-        //% block="group 8"
-        //% block.loc.nl="groep 8"
-        Group8,
-        //% block="group 9"
-        //% block.loc.nl="groep 9"
-        Group9
-    }
-
-    let GROUP = Group.Group1
-
     export enum Direction {
         //% block="none"
         //% block.loc.nl="geen"
@@ -202,8 +170,11 @@ namespace CXgoGamepad {
 
     //% block="join %group"
     //% block.loc.nl="sluit aan bij %group"
-    export function setGroup(group: Group) {
-        GROUP = group + 1
+    //$ group.min = 1 group.max=9
+    export function setGroup(group: number) {
+        if (group > 9) group = 9
+        if (group < 1) group = 1
+        GROUP = group
         radio.setGroup(GROUP)
     }
 
